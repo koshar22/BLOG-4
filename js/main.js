18f0cfb3e091e90cdefb8a774abfc4204a19c25f
koshar22 <saififiza454@gmail.com>
2021-10-30 15:02:38 +0530

    BLOG 4

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('tagg').addEventListener('mouseover', () => {
+    tagg.style.backgroundColor = 'blueviolet'
+})
+
+
+// document.getElementById('tagg').addEventListener('mouseout', () => {
+//     tagg.style.backgroundColor = '#d7e6eb'
+// })
