4dde100799f7e21396e18f210d878046ae217aa4
koshar22 <saififiza454@gmail.com>
2021-10-30 17:05:57 +0530

    BLOG 4

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
-document.getElementById('tagg').addEventListener('mouseover', () => {
-    tagg.style.backgroundColor = 'blueviolet'
+document.getElementById('tagg1').addEventListener('mouseover', () => {
+    tagg1.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg1').addEventListener('mouseout', () => {
+    tagg1.style.backgroundColor = '#d7e6eb'
 })
 
 
-// document.getElementById('tagg').addEventListener('mouseout', () => {
-//     tagg.style.backgroundColor = '#d7e6eb'
-// })
+document.getElementById('tagg2').addEventListener('mouseover', () => {
+    tagg2.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg2').addEventListener('mouseout', () => {
+    tagg2.style.backgroundColor = '#d7e6eb'
+})
+
+document.getElementById('tagg3').addEventListener('mouseover', () => {
+    tagg3.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg3').addEventListener('mouseout', () => {
+    tagg3.style.backgroundColor = '#d7e6eb'
+})
+
+
+document.getElementById('tagg4').addEventListener('mouseover', () => {
+    tagg4.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg4').addEventListener('mouseout', () => {
+    tagg4.style.backgroundColor = '#d7e6eb'
+})
+
+
+document.getElementById('tagg5').addEventListener('mouseover', () => {
+    tagg5.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg5').addEventListener('mouseout', () => {
+    tagg5.style.backgroundColor = '#d7e6eb'
+})
+
+
+document.getElementById('tagg6').addEventListener('mouseover', () => {
+    tagg6.style.backgroundColor = '#e056fd'
+})
+document.getElementById('tagg6').addEventListener('mouseout', () => {
+    tagg6.style.backgroundColor = '#d7e6eb'
+})
